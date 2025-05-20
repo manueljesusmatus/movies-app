c153ea26b3ce7cda8d7d7146bbfd5c6075c0baa3
Manuel Jesus Matus <manueljesusmatus@gmail.com>
2025-05-20 18:14:40 -0400

    fix: evitar imports wildcard

--- src/main/java/cl/dev/mmatush/moviesapp/controller/UtilController.java
+++ src/main/java/cl/dev/mmatush/moviesapp/controller/UtilController.java
@@ -4,7 +4,11 @@ import cl.dev.mmatush.moviesapp.model.dto.MovieDto;
 import cl.dev.mmatush.moviesapp.service.MovieQueuePublisher;
 import cl.dev.mmatush.moviesapp.service.ScraperService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
